b6c7c07147c9a1df6668be5d290ed8ead3c21d73
Deval0t <deval0t@outlook.com>
2024-04-03 13:39:45 +0200

    test
    test

---
+++ assets/js/hamburgerMenu.js
@@ -0,0 +1,39 @@
+const navToggler = document.querySelector(".navbar-toggler");
+const hamburger = document.querySelector(".hamburger-menu");
+
+const navLinks = document.querySelectorAll('.nav-item');
+const menuToggle = document.getElementById('navbarSupportedContent');
+const bsCollapse = new bootstrap.Collapse(menuToggle, {toggle:false});
+
+navToggler.addEventListener("click", function(e){
+
+    e.stopPropagation();
+    hamburger.classList.toggle('active');
+
+});
+
+menuToggle.addEventListener('shown.bs.collapse', function () {
+
+    hamburger.classList.add('active');
+
+});
+
+menuToggle.addEventListener('hidden.bs.collapse', function () {
+
+    hamburger.classList.remove('active');
+
+});
+
+navLinks.forEach((navLink) => {
+    navLink.addEventListener('click', (e) => {
+
+        e.stopPropagation();
+
+        // Check if the navbar toggler is visible (indicating mobile view)
+        if (getComputedStyle(navToggler).display !== 'none') {
+            bsCollapse.toggle(); // Toggle the mobile menu
+            hamburger.classList.remove('active');
+        }
+    });
+});
+
